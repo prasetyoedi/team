58b5cab9ebf99dc51cd966005f4c3f0049c7c510
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-08-11 20:23:16 +0700

    up blog dan blog post

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         loop: false,
@@ -6,15 +5,39 @@ $(document).ready(function () {
         nav: true,
         responsive: {
             0: {
-                items: 1
+                items: 1,
             },
             600: {
-                items: 2
+                items: 2,
             },
             1000: {
-                items: 4
-            }
-        }
+                items: 4,
+            },
+        },
     });
 });
 
+// Back to top button
+var backToTopBtn = document.getElementById("backToTopBtn");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+    ) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+backToTopBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
